11bc75e840bf6c1968c812932a7ded604d5bb481
PalCoat <palmis@edu.umea.se>
2023-04-25 15:22:07 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "../app.css";
+</script>
+
+<div>
+    <div class="flex-row">
+        <div>
+            <a class="btn btn-ghost normal-case text-xl flex-row flex" href="/">
+                <img src="icon.png" alt="">
+                <span>Typist</span>
+            </a>
+        </div>
+        <div class="menu flex-row px-1">
+            <li><a href="/Leaderboard"> Leaderboard</a></li>
+        </div>
+    </div>
+</div>
+
+<slot/>
