b0ebe7f8d40c10999a248b054ab7e41b0718a103
PalCoat <palmis@edu.umea.se>
2023-06-07 09:43:39 +0200

    Hope is low

--- src/lib/scripts/Socket.ts
+++ src/lib/scripts/Socket.ts
@@ -1,8 +1,10 @@
-let socketInstance : WebSocket;
+let socketInstance: WebSocket;
 
 export function getSocket(location: string, port: string) {
-  if (!socketInstance || socketInstance == undefined) {
-    socketInstance = new WebSocket("ws://" + location + ":" + port);
-  }
-  return socketInstance;
+    if (!socketInstance || socketInstance == undefined) {
+        socketInstance = new WebSocket(
+            "wss://typister.onrender.com" + ":" + port
+        );
+    }
+    return socketInstance;
 }
