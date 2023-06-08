89f7cda9ec325e8e4989b6ff6335eaa10d8eb03e
PalCoat <palmis@edu.umea.se>
2023-06-08 16:30:45 +0200

    Race 8

--- src/lib/scripts/Socket.ts
+++ src/lib/scripts/Socket.ts
@@ -2,7 +2,7 @@ let socketInstance: WebSocket;
 
 export function getSocket(location: string) {
     if (!socketInstance || socketInstance == undefined) {
-        socketInstance = new WebSocket("wss://" + location + ":" + "443");
+        socketInstance = new WebSocket("wss://" + location);
     }
     return socketInstance;
 }
